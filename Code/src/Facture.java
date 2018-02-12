4df4594f1f33d015bcc6857f7e87a70b05f54546
horyon <ludovic.noirault@imie.fr>
2018-02-12 11:16:21 +0100

    Classes revues et mises a jour on est good pou la mise en place de l'interface

--- Code/src/Facture.java
+++ Code/src/Facture.java
@@ -1,10 +1,41 @@
-public class Facture
-{
+package main;
 
+public class Facture {
 	private int id_Facture;
-	private int id_Devis;
-	private int id_Client;
-	private int totalProduits_Facture;
+	private Commande id_Commande;
+	private Client id_Client;
 	private Double totalFacture;
-
+	public Facture(int id_Facture, Commande id_Commande, Client id_Client, Double totalFacture) {
+		super();
+		this.id_Facture = id_Facture;
+		this.id_Commande = id_Commande;
+		this.id_Client = id_Client;
+		this.totalFacture = totalFacture;
+	}
+	public int getId_Facture() {
+		return id_Facture;
+	}
+	public void setId_Facture(int id_Facture) {
+		this.id_Facture = id_Facture;
+	}
+	public Commande getId_Commande() {
+		return id_Commande;
+	}
+	public void setId_Commande(Commande id_Commande) {
+		this.id_Commande = id_Commande;
+	}
+	public Client getId_Client() {
+		return id_Client;
+	}
+	public void setId_Client(Client id_Client) {
+		this.id_Client = id_Client;
+	}
+	public Double getTotalFacture() {
+		return totalFacture;
+	}
+	public void setTotalFacture(Double totalFacture) {
+		this.totalFacture = totalFacture;
+	}
+	
+	
 }
--- Code/src/adresseLivraison.java
+++ Code/src/adresseLivraison.java
@@ -1,8 +1,16 @@
-public class adresseLivraison
-{
-    
-    private int id_adresseLivraison;
-    private int code_postal_adresseLivraison;
-    private int code_postal_adresseLivraison;
+package main;
 
+public class AdresseLivraison extends Adresse{
+	private int id_Adresse_Livraison;
+	public AdresseLivraison(int numero, String rue, String ville, int code_postal, String pays,
+			int id_Adresse_Livraison) {
+		super(numero, rue, ville, code_postal, pays);
+		this.id_Adresse_Livraison = id_Adresse_Livraison;
+	}
+	public int getId_Adresse_Livraison() {
+		return id_Adresse_Livraison;
+	}
+	public void setId_Adresse_Livraison(int id_Adresse_Livraison) {
+		this.id_Adresse_Livraison = id_Adresse_Livraison;
+	}  
 }
