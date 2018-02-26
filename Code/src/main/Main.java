f3beedcb14b7bccdecf3fb5769cb211b1bc2e6fe
horyon <ludovic.noirault@imie.fr>
2018-02-26 16:46:39 +0100

    End day 26-02, connection a la base donnée effectuée, enregistrement des clients fonctionnent, prochainement ajout de produits etc

--- Code/src/main/Main.java
+++ Code/src/main/Main.java
@@ -93,7 +93,7 @@ public class Main
 		JPanel panelClient = new JPanel();
 		panelClient.setLayout(new BoxLayout(panelClient, BoxLayout.PAGE_AXIS));
 
-		fenetre2.setTitle("CrÃ©ation d'un nouveau client");
+		fenetre2.setTitle("Création d'un nouveau client");
 		fenetre2.setSize(700,250);
 		fenetre2.setLocation(200,120);
 
@@ -146,10 +146,10 @@ public class Main
 		panelAddLivr.add(numRueLivr);
 		panelAddLivr.add(labelrueLivr);
 		panelAddLivr.add(rueLivr);
-		panelAddLivr.add(labelcodePostalLivr);
-		panelAddLivr.add(codePostalLivr);
 		panelAddLivr.add(labelvilleLivr);
 		panelAddLivr.add(villeLivr);
+		panelAddLivr.add(labelcodePostalLivr);
+		panelAddLivr.add(codePostalLivr);
 		panelAddLivr.add(labelpaysLivr);
 		panelAddLivr.add(paysLivr);
 
@@ -183,10 +183,10 @@ public class Main
 		panelAddFact.add(numRueFact);
 		panelAddFact.add(labelrueFact);
 		panelAddFact.add(rueFact);
-		panelAddFact.add(labelcodePostalFact);
-		panelAddFact.add(codePostalFact);
 		panelAddFact.add(labelvilleFact);
 		panelAddFact.add(villeFact);
+		panelAddFact.add(labelcodePostalFact);
+		panelAddFact.add(codePostalFact);
 		panelAddFact.add(labelpaysFact);
 		panelAddFact.add(paysFact);
 
@@ -201,9 +201,27 @@ public class Main
 		{
 			public void actionPerformed(ActionEvent e)
 			{
+				int inptNumAdreFact = Integer.parseInt(numRueFact.getText());
+				String inptRueAdreFact = rueFact.getText();	
+				String inptVilleAdreFact = villeFact.getText();
+				int inptCpAdreFact =  Integer.parseInt(codePostalFact.getText());
+				String inptPaysAdreFact = paysFact.getText();
+				
+				int inptNumAdreLivr = Integer.parseInt(numRueLivr.getText());
+				String inptRueAdreLivr = rueLivr.getText();
+				String inptVilleAdreLivr = villeLivr.getText();
+				int inptCpAdreLivr =  Integer.parseInt(codePostalLivr.getText());
+				String inptPaysAdreLivr = paysLivr.getText();
+				
+				String inptNomClient = nomClient.getText();
+				String inptNomDirecteur= nomDirecteur.getText();
+				
 				ConnectToBDD connection = new ConnectToBDD();
 				connection.getConnection();
-				recuperInfosCreaClient();
+				recuperInfosCreaClient(
+						inptNumAdreFact,inptRueAdreFact,inptVilleAdreFact,inptCpAdreFact,inptPaysAdreFact,
+						inptNumAdreLivr,inptRueAdreLivr,inptVilleAdreLivr,inptCpAdreLivr,inptPaysAdreLivr,
+						inptNomClient, inptNomDirecteur);
 			}
 		});
 	}
@@ -267,16 +285,72 @@ public class Main
 //			}
 //	});
 	}
-	public static void recuperInfosCreaClient()
+	public static void recuperInfosCreaClient(
+			int fact1, String fact2,String fact3,int fact4,String fact5,
+			int livr1, String livr2,String livr3,int livr4,String livr5,
+			String clt1, String clt2)
 	{
+		int retourBDDFacture =0;
+		int retourBDDLivraison =0;
 		Connection connection  = ConnectToBDD.getConnection();
-		String sql = "INSERT INTO `produit` (`idProduit`, `nomProduit`, `prixuniProduit`) VALUES (NULL, 'pomme', '1.5')";
+		String sqlAdrFact = "INSERT INTO adressefacture (idAdresseFacture, numeroAdresseFacture, rueAdresseFacture, villeAdresseFacture, codepostalAdresseFacture, paysAdresseFacture) VALUES (NULL, "+fact1+", '"+fact2+"', '"+fact3+"', "+fact4+",'"+fact5+"')";
 		try
 		{
-			PreparedStatement ps = connection.prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			System.out.println("Requete SQL faite.");
-			rs.close();
+			PreparedStatement psAF = connection.prepareStatement(sqlAdrFact);
+			psAF.executeUpdate();
+			System.out.println("Requete SQL 1 faite.");
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		String sqlAdrLivr = "INSERT INTO adresselivaison (idAdresseLivraison, numeroAdresseLivraison, rueAdresseLivraison, villeAdresseLivraison, codepostalAdresseLivraison, paysAdresseLivraison)  VALUES (NULL, "+livr1+", '"+livr2+"', '"+livr3+"', "+livr4+",'"+livr5+"')";
+		try
+		{
+			PreparedStatement psAL = connection.prepareStatement(sqlAdrLivr);
+			psAL.executeUpdate();
+			System.out.println("Requete SQL 2 faite.");
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		String sqlGetIdFact = "SELECT idAdresseFacture FROM adresseFacture WHERE numeroAdresseFacture LIKE "+fact1+" AND rueAdresseFacture LIKE '"+fact2+"';";
+		try 
+		{
+			PreparedStatement psGetIdFact = connection.prepareStatement(sqlGetIdFact);
+			ResultSet  rs = psGetIdFact.executeQuery();
+			rs.next();
+			retourBDDFacture= rs.getInt("idAdresseFacture");
+			System.out.println("Requete SQL 3 faite.");
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		String sqlGetIdLivr = "SELECT idAdresseLivraison FROM adresseLivaison WHERE numeroAdresseLivraison LIKE "+livr1+" AND rueAdresseLivraison LIKE '"+livr2+"';";
+		try 
+		{
+			PreparedStatement psGetIdLivr = connection.prepareStatement(sqlGetIdLivr);
+			ResultSet  rs = psGetIdLivr.executeQuery();
+			rs.next();
+			retourBDDLivraison= rs.getInt("idAdresseLivraison");
+			System.out.println("Requete SQL 4 faite.");
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		String sqlClient = "INSERT INTO client (idClient, nomClient, nomDirigeantClient, idAdresseFacture, idAdresseLivraison ) VALUES (NULL, '"+clt1+"', '"+clt2+"', "+retourBDDFacture+", "+retourBDDLivraison+")";
+		try
+		{
+			PreparedStatement psC = connection.prepareStatement(sqlClient);
+			psC.executeUpdate();
+			System.out.println("Requete SQL 5 faite.");
+			connection.close();
 		}
 		catch (SQLException e)
 		{
